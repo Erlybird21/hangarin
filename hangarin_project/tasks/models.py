1380f26c7a55448b577a91f6bcf7fdae2f09352e
Erlybird21 <erljosephmangubat.phcs@gmail.com>
2026-08-22 00:46:21 +0800

    models and admin working properly

--- hangarin_project/tasks/models.py
+++ hangarin_project/tasks/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Priority(BaseModel):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Priorities"
+
+    def __str__(self):
+        return self.name
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Task(BaseModel):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ("Pending", "Pending"),
+        ("In Progress ", "In Progress"),
+        ("Completed", "Completed"),
+    ])
+    deadline = models.DateTimeField()
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    status = models.CharField(max_length=20, choices=[
+        ("Pending", "Pending"),
+        ("In Progress ", "In Progress"),
+        ("Completed", "Completed"),
+    ])
+
+    def __str__(self):
+        return self.title
+
+
+class Note(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Note for {self.task.title}"
